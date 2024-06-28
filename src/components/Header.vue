b5f7dd8a87f774d23d12a15196cdc17ca633d758
miku0129 <svartkatt39@gmail.com>
2024-06-28 19:17:45 +0200

    feat: update Header, add style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <script setup></script>
 
 <template>
-  <h2>Header</h2>
+  <div class="header-container">
+    <div>
+      <div class="header-title">UFCV</div>
+      <div>Dynamiser son pfojet professionnel</div>
+    </div>
+    <nav class="nav-router">
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/gallery">Gallery</RouterLink>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
-h2 {
-  color: gray;
-  background-color: antiquewhite;
+.header-container {
+  background-color: gainsboro;
+}
+.header-title {
+  font-size: xxx-large;
+}
+.nav-router {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 20px;
+  margin-right: 20px;
 }
 </style>
--- src/components/RouteHome.vue
+++ src/components/RouteHome.vue
@@ -1,9 +1,5 @@
 <template>
   <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-  <nav class="nav-router">
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/gallery">Go to Gallery</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
@@ -12,11 +8,6 @@
 <style scoped>
 h1 {
   color: gray;
-}
-.nav-router{
-  display: flex;
-  flex-direction: row;
-  gap: 20px; 
 }
 </style>
 
