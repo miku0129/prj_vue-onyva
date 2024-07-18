6b03a1f46650cda8c5398c398d1e6aa6bdee0c46
miku0129 <svartkatt39@gmail.com>
2024-07-18 12:23:45 +0200

    feat: update vite.config.ts to update baseurl

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -6,20 +6,16 @@ import Etudiant from "../components/Etudiant.vue";
 import EtudiantCreate from "../components/EtudiantCreate.vue";
 import EtudiantEdit from "../components/EtudiantEdit.vue";
 
-// import { getAllEtudiants } from "./helper";
-// const etudiantsData = await getAllEtudiants();
-
 const routes = [
   { path: "/", component: Home },
   { path: "/gallery/", component: Gallery },
   { path: "/etudiant/create/", component: EtudiantCreate },
-  // { path: "/etudiant/:id/", component: Etudiant , props: etudiantsData},
   { path: "/etudiant/:id/", component: Etudiant },
   { path: "/etudiant/:id/edit", component: EtudiantEdit },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/prj_vue-onyva/"),
+  history: createWebHashHistory(),
   routes,
 });
 
